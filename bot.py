f344a463752bd2ff655a0066ef00a8da534eadde
agarsol1 <agarsol1@epsa.upv.es>
2025-02-08 17:26:22 +0100

    Desarrollo del método para eliminar las tildes

--- bot.py
+++ bot.py
@@ -1,2 +1,42 @@
-import numpy as np
+import string as str
 
+with open("diccionario-rae-completo.txt", "r", encoding="utf-8") as file:
+    palabras = [line.strip() for line in file]
+
+print(f"Total de palabras: {len(palabras)}")
+
+
+def convertidor_ascii(letra):
+    x=letra.encode()
+    return int.from_bytes(x, byteorder="big")
+
+for letra1 in "aáeéiíoóuú":
+    print(f"Letra {letra1} = {convertidor_ascii(letra1)}")
+
+
+
+def eliminar_tilde(palabra):
+    print(f"PALABRA INICIAL: {palabra}")
+    con_sin_tilde = {
+        "á" : "a",
+        "é" : "e",
+        "í" : "i",
+        "ó" : "o",
+        "ú" : "u",
+        "Á" : "A",
+        "É" : "E",
+        "Í" : "I",
+        "Ó" : "O",
+        "Ú" : "U"
+    }
+
+    palabra_nueva = "".join(con_sin_tilde.get(letra, letra) for letra in palabra)
+    
+    print(f"PALABRA FINAL: {palabra_nueva}")
+
+print(eliminar_tilde(palabras[14]))
+
+
+        
+
+    
