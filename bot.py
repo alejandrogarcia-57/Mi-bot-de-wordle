4f070c7f1616447928b7e71e5f2bc2d84e9e1643
agarsol1 <agarsol1@epsa.upv.es>
2025-02-09 17:17:34 +0100

    Desarrollo de los métodos de búsqueda y filtro de palabras

--- bot.py
+++ bot.py
@@ -1,18 +1,10 @@
 import string as str
+import re
 
 with open("diccionario-rae-completo.txt", "r", encoding="utf-8") as file:
     palabras = [line.strip() for line in file]
 
 print(f"Total de palabras: {len(palabras)}")
-
-
-def convertidor_ascii(letra):
-    x=letra.encode()
-    return int.from_bytes(x, byteorder="big")
-
-for letra1 in "aáeéiíoóuú":
-    print(f"Letra {letra1} = {convertidor_ascii(letra1)}")
-
 
 
 def eliminar_tilde(palabra):
@@ -35,6 +27,49 @@ def eliminar_tilde(palabra):
     print(f"PALABRA FINAL: {palabra_nueva}")
 
 print(eliminar_tilde(palabras[14]))
+
+def tamano_palabra(size, palabras_actual):
+    nueva_array_palabras = [palabra for palabra in palabras_actual if len(palabra) == size]
+    return nueva_array_palabras
+     
+
+def resultado_elecc(resultado, palabra_eleg):
+    res_intento = []
+    for p in resultado:
+        if p == "B":
+            res_intento.append(palabra_eleg[p])
+        if p == "M" and "C":
+            res_intento.append("_")
+
+    return res_intento
+
+def filtrar_palabras(res_intento, palabras_actual):
+    cont = 0
+    for let in res_intento:
+        if let != "_":
+            cont += 1
+    
+    if cont == 0:
+        return palabras_actual
+    else:
+        res_intento_regex = "^" + res_intento.replace("_", ".") + "$"
+
+        palabras_filtradas = [palabra for palabra in palabras_actual if re.match(res_intento_regex, palabra)]
+        return palabras_filtradas
+
+        
+
+
+        
+
+def main():
+
+    print("#############################################")
+    print("#                                           #")
+    print("#               BOT DE WORDLE               #")
+    print("#                                           #")
+    print("#############################################")
+
 
 
         
